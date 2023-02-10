12df585ac84593e5a3e5d9db2cd577230c107ff3
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-02-10 00:19:29 -0300

    Cadastrando contatos

--- ControleDeContatos/Controllers/Contato.cs
+++ ControleDeContatos/Controllers/Contato.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeContatos.Controllers
 {
     public class Contato : Controller
     {
+        private readonly IContatoRepositorio _contatoRepositorio;
+        public Contato(IContatoRepositorio contatoRepositorio) {
+            _contatoRepositorio = contatoRepositorio;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -22,6 +29,13 @@ namespace ControleDeContatos.Controllers
         public IActionResult ApagarConfirmacao()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato) {
+
+            _contatoRepositorio.Adicionar(contato);
+            return RedirectToAction("Index");
         }
 
     }
--- ControleDeContatos/Data/BancoContext.cs
+++ ControleDeContatos/Data/BancoContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,11 @@ namespace ControleDeContatos.Data
 {
     public class BancoContext : DbContext
     {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options) {
+
+        }
+        
+
+        public DbSet<ContatoModel> Contatos { get; set; }
     }
 }
